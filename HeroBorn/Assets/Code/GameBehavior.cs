64438fd04e7744ef68a3c34a6561d6c3a0430778
NicoAccomazzo <nicoaccomazzo@gmail.com>
2022-02-16 14:05:27 -0700

    Chapter 9 stuff

---
+++ HeroBorn/Assets/Code/GameBehavior.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameBehavior : MonoBehaviour
+{
+    public string labelText = "Collect all 5 items and win your freedom!";
+    public int maxItems = 5;
+    public bool showWinScreen = false;
+    private int _itemsCollected = 0;
+    public int Items {
+        get {return _itemsCollected;}
+        set {
+            _itemsCollected = value;
+            if(_itemsCollected >= maxItems) {
+                labelText = "You've found all the items!";
+                showWinScreen = true;
+
+                Time.timeScale = 0f;
+            } else {
+                labelText = "item found, only " + (maxItems - _itemsCollected) + " more to go!";
+            }
+            Debug.LogFormat("Items: {0}", _itemsCollected);
+        }
+    }
+
+    private int _playerHP = 10;
+    public int HP {
+        get {return _playerHP;}
+        set {
+            _playerHP = value;
+            Debug.LogFormat("Health: {0}", _playerHP);
+        }
+    }
+
+    void OnGUI() {
+        GUI.Box(new Rect(20, 20, 150, 25), "Player Health:" + _playerHP);
+        GUI.Box(new Rect(20, 50, 150, 25), "Items:" + _itemsCollected);
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 30), labelText);
+
+        if (showWinScreen)
+        {
+            if (GUI.Button(new Rect(Screen.width / 2 -100, Screen.height / 2 - 50, 200, 100), "You've collected all the Items!"))
+            {
+                SceneManager.LoadScene(0);
+                Time.timeScale = 1.0f;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- HeroBorn/Assets/Code/PlayerBehavior.cs
+++ HeroBorn/Assets/Code/PlayerBehavior.cs
@@ -4,36 +4,79 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    public float moveSpeed = 10f;
+    public float moveSpeed = 3f;
     public float rotateSpeed = 100f;
 
+    public float jumpVelocity = 3f;
+    public float distanceToGround = 0.01f;
+    public LayerMask groundLayer;
+
+    public GameObject bullet;
+    public float bulletSpeed = 100f;
+    private bool doShoot;
+
+    private bool doJump;
     private float vInput;
     private float hInput;
     private Rigidbody _rb;
+    private CapsuleCollider _col;
 
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
+
+        _col = GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            doJump = true;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            doShoot = true;
+        }
+
         vInput = Input.GetAxis("Vertical") * moveSpeed;
         hInput = Input.GetAxis("Horizontal") * rotateSpeed;
         
-        /*
-        this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
-        this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
-        */
+        
     }
 
     void FixedUpdate()
     {
-            Vector3 rotation = Vector3.up * hInput;
-            Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
-            _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
-            _rb.MoveRotation(_rb.rotation * angleRot);
+        Vector3 rotation = Vector3.up * hInput;
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
+        _rb.MoveRotation(_rb.rotation * angleRot);
+
+
+        // Jumping when flag is triggered
+        if (doJump)
+        {
+            _rb.AddForce(Vector3.up * jumpVelocity, ForceMode.Impulse);
+            doJump = false;
+        }
+
+        if (doShoot)
+        {
+            GameObject newBullet = Instantiate (bullet, this.transform.position + new Vector3(1, 0, 0), this.transform.rotation) as GameObject;
+            Rigidbody bulletRB = newBullet.GetComponent<Rigidbody>();
+            bulletRB.velocity = this.transform.forward * bulletSpeed;
+            doShoot = false;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3 (_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
+        bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, distanceToGround, groundLayer, QueryTriggerInteraction.Ignore);
+        
+        return grounded;
     }
 }
